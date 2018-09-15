415b776bd9cfb0cf952dcbbd04d364c441d79991
Chris Choi <choicd@hotmail.com>
2018-09-15 17:24:24 -0400

    Second commit

--- hello.go
+++ hello.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
+import ("fmt"
+        "github.com/cdchoi/stringutil"
+)
 
 func main () {
-    fmt.Printf("Hello, workd\n")
+    fmt.Printf(stringutil.Reverse("!oG , olleH"))
 }
